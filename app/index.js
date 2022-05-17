6a6f7db94feba72e55524515dd091bbb8fc2497b
Juan Ceballos <jacu29@gmail.com>
2022-05-16 19:37:24 -0500

    First Sprint

--- app/index.js
+++ app/index.js
@@ -3,13 +3,15 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 const databaseConnection = require('./config/database');
 
 const app = express();
-
+const accessLogStream = fs.createWriteStream(path.resolve("access.log"), {flags: 'a'});
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./app/uploads'));
